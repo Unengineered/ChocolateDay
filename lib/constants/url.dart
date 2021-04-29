e86273aa276e02efb9788bb827a1edeecb097ada
Advait <advaitbansode4@gmail.com>
2021-04-29 11:58:12 +0530

    FIX: changed URLs to production values

--- lib/constants/url.dart
+++ lib/constants/url.dart
@@ -1,9 +1,9 @@
-const kAWSEmailsBaseUrl = "http://localhost/emails";
-const kUserDataUrl = "http://localhost/userInfo";
-const kRazorPayUrl = "http://localhost/razorpay";
-const kCreateAccountUrl = "http://localhost/register";
+// const kAWSEmailsBaseUrl = "http://localhost/emails";
+// const kUserDataUrl = "http://localhost/userInfo";
+// const kRazorPayUrl = "http://localhost/razorpay";
+// const kCreateAccountUrl = "http://localhost/register";
 
-// const kAWSEmailsBaseUrl = "https://chocolateday.in/emails";
-// const kUserDataUrl = "https://chocolateday.in/userInfo";
-// const kRazorPayUrl = "https://chocolateday.in/razorpay";
-// const kCreateAccountUrl = "https://chocolateday.in/register";
+const kAWSEmailsBaseUrl = "https://chocolateday.in/emails";
+const kUserDataUrl = "https://chocolateday.in/userInfo";
+const kRazorPayUrl = "https://chocolateday.in/razorpay";
+const kCreateAccountUrl = "https://chocolateday.in/register";
