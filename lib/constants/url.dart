4b636507bbba1c0918894d85409e2067ffe73488
Advait <advaitbansode4@gmail.com>
2021-04-29 15:34:45 +0530

    FEATURE: Added favicon

--- lib/constants/url.dart
+++ lib/constants/url.dart
@@ -1,9 +1,9 @@
-const kAWSEmailsBaseUrl = "http://localhost/emails";
-const kUserDataUrl = "http://localhost/userInfo";
-const kRazorPayUrl = "http://localhost/razorpay";
-const kCreateAccountUrl = "http://localhost/register";
+// const kAWSEmailsBaseUrl = "http://localhost/emails";
+// const kUserDataUrl = "http://localhost/userInfo";
+// const kRazorPayUrl = "http://localhost/razorpay";
+// const kCreateAccountUrl = "http://localhost/register";
 
-// const kAWSEmailsBaseUrl = "https://www.chocolateday.in/emails";
-// const kUserDataUrl = "https://www.chocolateday.in/userInfo";
-// const kRazorPayUrl = "https://www.chocolateday.in/razorpay";
-// const kCreateAccountUrl = "https://www.chocolateday.in/register";
+const kAWSEmailsBaseUrl = "https://www.chocolateday.in/emails";
+const kUserDataUrl = "https://www.chocolateday.in/userInfo";
+const kRazorPayUrl = "https://www.chocolateday.in/razorpay";
+const kCreateAccountUrl = "https://www.chocolateday.in/register";
