9a57d97248db169f1f7711ff4604af0c8986730b
Advait <advaitbansode4@gmail.com>
2021-05-09 02:57:30 +0530

    FEATURE: Added gesture detector to home aasra foundation card

--- lib/components/signing/aasra_home_card.dart
+++ lib/components/signing/aasra_home_card.dart
@@ -8,38 +8,44 @@ class AasraHomeCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: MediaQuery.of(context).size.width * 0.6,
-      decoration: BoxDecoration(
-          color: Color.fromRGBO(32, 33, 92, 1),
-          borderRadius: BorderRadius.circular(18),
-          boxShadow: [
-            BoxShadow(
-              color: Color(0x085E636B),
-              offset: Offset(0, 12),
-              blurRadius: 16,
+    return GestureDetector(
+      onTap: () {
+        Navigator.of(context).pushNamed('/aasra');
+      },
+      child: Container(
+        width: MediaQuery.of(context).size.width * 0.6,
+        decoration: BoxDecoration(
+            color: Color.fromRGBO(32, 33, 92, 1),
+            borderRadius: BorderRadius.circular(18),
+            boxShadow: [
+              BoxShadow(
+                color: Color(0x085E636B),
+                offset: Offset(0, 12),
+                blurRadius: 16,
+              )
+            ]),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Image.asset(
+                'asset/illustrations/aasra_logo2.png',
+                height: 100,
+                width: 100,
+              ),
+            ),
+            SizedBox(width: 1),
+            Expanded(
+              child: Text(
+                "Donate to Aasra fundraiser.",
+                style:
+                    kSubtitleStyle.copyWith(color: Colors.white, fontSize: 12),
+                textAlign: TextAlign.center,
+              ),
             )
-          ]),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Padding(
-            padding: EdgeInsets.all(8.0),
-            child: Image.asset(
-              'asset/illustrations/aasra_logo2.png',
-              height: 100,
-              width: 100,
-            ),
-          ),
-          SizedBox(width: 1),
-          Expanded(
-            child: Text(
-              "Donate to Aasra fundraiser.",
-              style: kSubtitleStyle.copyWith(color: Colors.white, fontSize: 12),
-              textAlign: TextAlign.center,
-            ),
-          )
-        ],
+          ],
+        ),
       ),
     );
   }
