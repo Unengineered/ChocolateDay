53cf1e288b605bd6c251b13d26c51f6af74d8e63
Advait <advaitbansode4@gmail.com>
2021-05-08 19:07:03 +0530

    FEATURE: Added Aasra fundraiser page

--- lib/constants/url.dart
+++ lib/constants/url.dart
@@ -1,11 +1,11 @@
-const kAWSEmailsBaseUrl = "http://localhost:3000/emails";
-const kUserDataUrl = "http://localhost:3000/userInfo";
-const kRazorPayUrl = "http://localhost:3000/razorpay";
-const kCreateAccountUrl = "http://localhost:3000/register";
-const kDonationInfoUrl = "http://localhost:3000/donations";
+// const kAWSEmailsBaseUrl = "http://localhost:3000/emails";
+// const kUserDataUrl = "http://localhost:3000/userInfo";
+// const kRazorPayUrl = "http://localhost:3000/razorpay";
+// const kCreateAccountUrl = "http://localhost:3000/register";
+// const kDonationInfoUrl = "http://localhost:3000/donations";
 
-// const kAWSEmailsBaseUrl = "https://www.chocolateday.in/emails";
-// const kUserDataUrl = "https://www.chocolateday.in/userInfo";
-// const kRazorPayUrl = "https://www.chocolateday.in/razorpay";
-// const kCreateAccountUrl = "https://www.chocolateday.in/register";
-// const kDonationInfoUrl = "https://www.chocolateday.in/donations";
+const kAWSEmailsBaseUrl = "https://www.chocolateday.in/emails";
+const kUserDataUrl = "https://www.chocolateday.in/userInfo";
+const kRazorPayUrl = "https://www.chocolateday.in/razorpay";
+const kCreateAccountUrl = "https://www.chocolateday.in/register";
+const kDonationInfoUrl = "https://www.chocolateday.in/donations";
